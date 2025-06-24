f16f3fa9b9de05d547357b44cb88eb8073905c23
Shreeshalyik <shreesha@lyik.com>
2025-06-24 10:29:29 +0530

    feat: add sample JWT token generation script

---
+++ ttk_plugin/src/lyik/ttk/auth/samples/sample_token_gen.py
@@ -0,0 +1,39 @@
+import datetime
+import jwt
+
+TTK_TOKEN_SECRET = "BmThdDKu4lPOFiiqwHG1GQVm7iGeVIqALcqwJDM6VySzVdOwZ1UMoylzwhIDgXl6"
+
+
+payload = {
+    "userId": "user_100",
+    "accessType": "maker",
+    "fullName": "Jhon Doe",
+    "loginTime": "2025-04-25 15:10:30",
+    "expiryTimestamp": "1781934720",
+    "order_id":  "abc_123"
+    # "relationship": ["user_xyz", "user_1222"],
+    # "iss": "https://auth.ttk.com/",
+    # "aud": "https://lyik.com",
+}
+
+token = jwt.encode(
+    payload,
+    TTK_TOKEN_SECRET,
+    algorithm="HS256",
+)
+
+# If token is bytes, decode it to str
+if isinstance(token, bytes):
+    token = token.decode('utf-8')
+
+print("🔐 JWT Token:")
+print(token)
+
+deco = jwt.decode(
+    token,
+    TTK_TOKEN_SECRET,
+
+    algorithms=["HS256"],
+)
+
+print(deco)
