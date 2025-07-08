f63524e199d767d366441ae05874ff2e6a8c357c
Akhil Babu <93635893+AkhilBabuM@users.noreply.github.com>
2025-07-08 13:14:53 +0530

    template: Add Jinja2 template for payment request

--- deployment_templates/templates/py_modules/filter_utility.py
+++ deployment_templates/templates/py_modules/filter_utility.py
@@ -1,3 +1,6 @@
 import base64
 def to_base64(s):
     return base64.b64encode(s.encode("utf-8")).decode("utf-8")
+
+def to_base64url(s):
+    return base64.urlsafe_b64encode(s.encode("utf-8")).decode("utf-8")
