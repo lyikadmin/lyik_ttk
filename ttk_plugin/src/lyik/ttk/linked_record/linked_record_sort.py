5722ca3a3ab1a3f8cc4c162ce43fcc26b51fd0a7
Debasish Sarkar <debasish@lyik.com>
2025-07-27 16:37:49 +0530

    custom linked record sort plugin impl

---
+++ ttk_plugin/src/lyik/ttk/linked_record/linked_record_sort.py
@@ -0,0 +1,29 @@
+import logging
+from collections import OrderedDict
+
+from typing_extensions import Annotated, Doc
+from lyikpluginmanager import ContextModel
+import apluggy as pluggy
+
+from lyikpluginmanager import getProjectName
+from lyikpluginmanager.core import LinkRecordSortSpec, SortOrder
+
+logger = logging.getLogger(__name__)
+
+impl = pluggy.HookimplMarker(getProjectName())
+
+
+class LinkedRecordSortPlugin(LinkRecordSortSpec):
+    @impl
+    async def sort_linked_records(self, context: ContextModel) -> Annotated[
+        OrderedDict | None,
+        Doc(
+            "This is the sort mapping, where key is the source field name and value is the sort order."
+        ),
+    ]:
+        sort_mapping = OrderedDict()
+
+        sort_mapping["Traveller Type"] = SortOrder.DESC
+        sort_mapping["Order ID"] = SortOrder.ASC
+
+        return sort_mapping
