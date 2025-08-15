6a3cefa04fef210387605f1253e1d4a4bceabd38
Debasish Sarkar <debasish@lyik.com>
2025-08-16 00:45:00 +0530

    added travel insurance verifier

---
+++ ttk_plugin/src/lyik/ttk/travel_insurance_verifier/travel_insurance_verifier.py
@@ -0,0 +1,65 @@
+import apluggy as pluggy
+from lyikpluginmanager import (
+    getProjectName,
+    ContextModel,
+    PluginException,
+    VerifyHandlerSpec,
+    VerifyHandlerResponseModel,
+    VERIFY_RESPONSE_STATUS,
+)
+from typing import Annotated
+from typing_extensions import Doc
+from lyik.ttk.models.forms.schengentouristvisa import RootTravelInsurance
+import logging
+from lyik.ttk.utils.message import get_error_message
+
+logger = logging.getLogger(__name__)
+
+impl = pluggy.HookimplMarker(getProjectName())
+
+ACTOR = "system"
+
+
+class TravelInsuranceVerifier(VerifyHandlerSpec):
+
+    @impl
+    async def verify_handler(
+        self,
+        context: ContextModel,
+        payload: Annotated[
+            RootTravelInsurance,
+            Doc("Travel Insurance payload."),
+        ],
+    ) -> Annotated[
+        VerifyHandlerResponseModel,
+        Doc("Response after validating the Travel Insurance."),
+    ]:
+        """
+        This verifier validates the data of the Travel Insurance section.
+        """
+        skip_travel_insurance = None
+        if payload and payload.add_on_service_option:
+            skip_travel_insurance = payload.add_on_service_option
+
+        salary_slip_file = None
+        if (
+            payload
+            and payload.flight_reservation_details
+            and payload.flight_reservation_details.flight_reservation_tickets
+        ):
+            salary_slip_file = (
+                payload.flight_reservation_details.flight_reservation_tickets
+            )
+
+        if not (skip_travel_insurance or salary_slip_file):
+            return VerifyHandlerResponseModel(
+                actor=ACTOR,
+                message=get_error_message("LYIK_ERR_NO_TRAVEL_INSURANCE"),
+                status=VERIFY_RESPONSE_STATUS.FAILURE,
+            )
+
+        return VerifyHandlerResponseModel(
+            actor=ACTOR,
+            message=f"Verified by {ACTOR}",
+            status=VERIFY_RESPONSE_STATUS.SUCCESS,
+        )
